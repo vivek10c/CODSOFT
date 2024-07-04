8928d2795680b69b3e8fea0082fe44c629328257
vivek10c <124761412+vivek10c@users.noreply.github.com>
2024-07-04 16:55:28 +0530

    Update main.js

--- Task1-Portfolio/main.js
+++ Task1-Portfolio/main.js
@@ -5,3 +5,17 @@ var typed= new Typed(".text",{
     backDelay:1000,
     loop:true
 })
+
+function opentab(tabname) {
+    var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+    for (var tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (var tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    document.getElementById(tabname).classList.add("active-tab");
+    event.currentTarget.classList.add("active-link");
+}
